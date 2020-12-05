fc1f4036d607c8d853628db3cd56c44759928db1
Rodrigo Caetano Reynaldo <pp5ere@gmail.com>
2020-12-05 11:18:34 -0300

    It was removed unused functions

--- repository/repository.go
+++ repository/repository.go
@@ -2,10 +2,7 @@ package repository
 
 import (
 	"Weather/entity"
-	"Weather/middlewares/util"
 	"Weather/repository/log"
-	"context"
-	"database/sql"
 	"strconv"
 	"time"
 )
@@ -21,17 +18,13 @@ type Weather interface{
 //Insert add a new weather into db
 func (c *SqliteDB) Insert(w *entity.Weather) error {
 	connection := c.connection
-	addColumnIfNotExist("hi", c, updateHeatIndex)
-	addColumnIfNotExist("dewpoint", c, updateDewPoint)
-	dp := util.DewPoint(w.TempC,w.Hum)
-	hiC := util.FahrenheitToCelsius(util.CalculateHeatIndex(w.TempF,w.Hum))
-	res, err := connection.Exec(`insert into weather(data, tempc, tempf, hum, pres, alt, hi, dewpoint) values(?,?,?,?,?,?,?,?)`, time.Now(),w.TempC,w.TempF,w.Hum,w.Pres,w.Alt,hiC, dp);if err != nil {
+	res, err := connection.Exec(`insert into weather(data, tempc, tempf, hum, pres, alt, hi, dewpoint) values(?,?,?,?,?,?,?,?)`, time.Now(),w.TempC,w.TempF,w.Hum,w.Pres,w.Alt,w.Hi, w.DewPoint);if err != nil {
 		return err
 	}
 	id, err := res.LastInsertId();if err != nil {
 		return err
 	}
-	return insertLog(id, w, dp, hiC)
+	return insertLog(id, w)
 }
 
 //FindAll returns all Weather from database order by ID
@@ -89,94 +82,19 @@ func (c *SqliteDB) FindMaxMinTempCPerDay(d time.Time) ([]*entity.WeatherMaxMin, 
 	return weathers, err
 }
 
-func insertLog(id int64, w *entity.Weather, dp, hiC float64) error {
+func insertLog(id int64, w *entity.Weather) error {
 	var msg string
 	msg = "Execute insert into weather: ID: " + strconv.FormatInt(id, 10) +
 			 " TempC = " + strconv.FormatFloat(w.TempC, 'f', 2, 64) + 
-			 " TempF: " + strconv.FormatFloat(w.TempF, 'f', 2, 64) + 
-			 " Hum: " + strconv.FormatFloat(w.Hum,'f',2,64) + 
-			 " Pres: " + strconv.FormatFloat(w.Pres,'f',2,64) + 
-			 " Alt: " + strconv.FormatFloat(w.Alt,'f',2,64)+
-			 " Hi: " + strconv.FormatFloat(hiC,'f',2,64)+
-			 " DewPoint: " + strconv.FormatFloat(dp,'f',2,64)
+			 " TempF = " + strconv.FormatFloat(w.TempF, 'f', 2, 64) + 
+			 " Hum = " + strconv.FormatFloat(w.Hum,'f',2,64) + 
+			 " Pres = " + strconv.FormatFloat(w.Pres,'f',2,64) + 
+			 " Alt = " + strconv.FormatFloat(w.Alt,'f',2,64)+
+			 " Hi = " + strconv.FormatFloat(w.Hi,'f',2,64)+
+			 " DewPoint = " + strconv.FormatFloat(w.DewPoint,'f',2,64)
 	return log.WriteLog(msg)
 }
 
-func addColumnIfNotExist(colName string, c *SqliteDB, f func(*sql.Tx)error) (*sql.Tx, error) {
-	var exist bool
-	var tx *sql.Tx
-	connection := c.connection
-	col := "%"+colName+"%"
-	err := connection.QueryRow(`SELECT EXISTS (SELECT * FROM sqlite_master WHERE tbl_name = 'weather' AND sql LIKE ?)as exist`, col).Scan(&exist); if err != nil {
-		log.WriteLog(err.Error())
-		return tx, err
-	}
-	if !exist  {
-		ctx := context.Background()
-		tx, err = connection.BeginTx(ctx, nil); if err != nil {
-			log.WriteLog(err.Error())
-			return tx, err
-		}
-		_, err = tx.ExecContext(ctx, `alter table weather add column ` + colName + ` float;`);if err != nil {
-			tx.Rollback()
-			log.WriteLog(err.Error())
-			return tx, err
-		}
-			
-		//err = updateHeatIndex(tx);if err != nil {
-		err = f(tx);if err != nil {
-			tx.Rollback()
-			log.WriteLog(err.Error())
-			return tx, err
-		}
-		err = tx.Commit();if err != nil {
-			log.WriteLog(err.Error())
-			return tx, err
-		}
-	}
-	
-	return tx, err
-}
-
-func updateHeatIndex(c *sql.Tx) error{
-	rows, err := c.Query(`select id, tempf, hum from weather where hi is null order by id;`);if err != nil {
-		log.WriteLog(err.Error())
-		return err
-	}
-	for rows.Next(){
-		var w entity.Weather
-		err := rows.Scan(&w.ID, &w.TempF, &w.Hum);if err != nil {
-			log.WriteLog(err.Error())
-			return err
-		}
-		hiC := util.FahrenheitToCelsius(util.CalculateHeatIndex(w.TempF, w.Hum))
-		_, err = c.Exec("update weather set hi = ? where id=?;", hiC, w.ID);if err != nil {
-			log.WriteLog(err.Error())
-			return err
-		}
-	}
-	return err
-}
-
-func updateDewPoint(c *sql.Tx) error{
-	rows, err := c.Query(`select id, tempc, hum from weather where dewpoint is null order by id;`);if err != nil {
-		log.WriteLog(err.Error())
-		return err
-	}
-	for rows.Next(){
-		var w entity.Weather
-		err := rows.Scan(&w.ID, &w.TempC, &w.Hum);if err != nil {
-			log.WriteLog(err.Error())
-			return err
-		}
-		dp := util.DewPoint(w.TempC,w.Hum)
-		_, err = c.Exec("update weather set dewpoint = ? where id=?;", dp, w.ID);if err != nil {
-			log.WriteLog(err.Error())
-			return err
-		}
-	}
-	return err
-}
 //CreateTable create table weather if it not exist
 func (c *SqliteDB) CreateTable(dbName string) error {
 	var exist bool
