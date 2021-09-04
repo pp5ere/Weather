f9d9a7e61ae2dcbbb8caeb22a0b8b4530c851296
Rodrigo Caetano Reynaldo <pp5ere@gmail.com>
2021-09-04 09:40:02 -0300

    It was added error and Reactappfolder

--- usecase/router.go
+++ usecase/router.go
@@ -1,8 +1,11 @@
 package usecase
 
 import (
-	"github.com/gorilla/mux"
 	"Weather/controller"
+	"Weather/helper"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 
@@ -11,12 +14,16 @@ func (a *API) endPoints()  {
 	a.Router.HandleFunc("/weather/{data}", a.findByData)
 	a.Router.HandleFunc("/maxmintemp/{data}", a.findMaxMinTempCPerDay)
 	a.Router.HandleFunc("/iotdata", a.findDataFromIoT)
+	a.Router.PathPrefix("/").Handler(http.StripPrefix("/", http.FileServer(http.Dir(a.ReactAppFolder))))
 }
 
 //Initialize the route
-func Initialize(w *controller.Controllers) *mux.Router {
+func Initialize(w *controller.Controllers) (*mux.Router, error) {
+	c, err := helper.LoadFromConfigFile();if err != nil {
+		return nil, err
+	}
 	thisRoute := mux.NewRouter()
-	thisAPI := &API{Router: thisRoute,Weather: w.Weather}
+	thisAPI := &API{Router: thisRoute,Weather: w.Weather, ReactAppFolder: c.ReactAppFolder}
 	thisAPI.endPoints()	
-	return thisRoute
+	return thisRoute, nil
 }
